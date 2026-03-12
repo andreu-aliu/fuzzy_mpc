9f0f5d5cc61cc3e8c199fa973163d61cc505b320
Andreu <gualisi@gmail.com>
2026-03-12 10:45:27 +0100

    Save model to yaml

--- mtlb/models/ltv_tv/ltv_tv_matrix.m
+++ mtlb/models/ltv_tv/ltv_tv_matrix.m
@@ -1,43 +1,67 @@
-% The funciton returns the discrete state matrices for a given state (predicted + vx)
-% x = A x + B u + C
+% The function returns discrete next-state matrices for a given predicted state/input
+% x_{k+1} = Ad x_k + Bd u_k + Cd
+%
 % x: [y vy psi r]
 % u: [st mz]
+
 function [Ad, Bd, Cd] = ltv_tv_matrix(X_pred, U_pred, vx)
 
 dt = 0.01;
 
+% Predicted operating point
+psi   = X_pred(3);
+delta = U_pred(1);
+
 % Car parameters
-m = 220;
+m  = 220;
 Iz = 188;
 lf = 0.765;
 lr = 0.765;
 Cf = 1.2705 * 10.5507 * 1104.0;
 Cr = 1.2705 * 10.5507 * 1281.5;
-Rw = 0.2032;
-rho = 1.225;
-SCd = 1.854;
 
-% Model matrix (y, vy, psi, r)
-if vx < 2
-    A = zeros(4); 
-    B = zeros(4,2);
-else
-    A = [0, 1, vx, 0; % cos(psi), vx*cos(psi), 0;
-         0, -(Cf*cos(delta)+Cr)/(m*vx), 0, -((lf*Cf*cos(delta)-lr*Cr)/(m*vx))+vx;
-         0, 0, 0, 1;
-         0, -(lf*Cf*cos(delta)-lr*Cr)/(Iz*vx), 0, -(lf*lf*Cf*cos(delta)+lr*lr*Cr)/(Iz*vx)];
-    B = [0, 0;
-         Cf*cos(delta)/m, 0;
-         0, 0;
-         lf*Cf*cos(delta)/Iz, 1/Iz];
-end
+% Avoid division by zero / bad conditioning at very low speed
+vx_eff = max(vx, 0.5);
 
-% Discretize matrix
-M = [A B; zeros(2,4), zeros(2,2)];
-exp_matrix = expm(M*dt);
-Ad = exp_matrix(1:4,1:4);
-Bd = exp_matrix(1:4,5:6);
-Cd = zeros(4,1);
+% Continuous-time affine model:
+% xdot = Ac*x + Bc*u + Cc
 
-Ad = Ad + eye(4);
+Ac = zeros(4,4);
+Bc = zeros(4,2);
+Cc = zeros(4,1);
+
+% y kinematics, same style as direct_anfis_matrix
+% y_dot = vy*cos(psi) + vx*sin(psi)
+Ac(1,2) = cos(psi);
+Cc(1)   = vx * sin(psi);
+
+% vy dynamics
+Ac(2,2) = -(Cf*cos(delta) + Cr) / (m * vx_eff);
+Ac(2,4) = -((lf*Cf*cos(delta) - lr*Cr) / (m * vx_eff)) + vx_eff;
+Bc(2,1) =  Cf*cos(delta) / m;
+Bc(2,2) =  0;
+
+% psi kinematics
+Ac(3,4) = 1;
+
+% r dynamics
+Ac(4,2) = -(lf*Cf*cos(delta) - lr*Cr) / (Iz * vx_eff);
+Ac(4,4) = -(lf^2*Cf*cos(delta) + lr^2*Cr) / (Iz * vx_eff);
+Bc(4,1) =  lf*Cf*cos(delta) / Iz;
+Bc(4,2) =  1 / Iz;
+
+% Exact discretization of affine system
+% [x_{k+1}]   [Ad Bd Cd] [x_k]
+% [   1    ] = [ 0  1  0] [u_k]
+%                         [ 1 ]
+M = [Ac, Bc, Cc;
+     zeros(2,4), zeros(2,2), zeros(2,1);
+     zeros(1,4), zeros(1,2), 0];
+
+expM = expm(M * dt);
+
+Ad = expM(1:4, 1:4);
+Bd = expM(1:4, 5:6);
+Cd = expM(1:4, 7);
+
 end
---
+++ mtlb/utils/save_yaml.m
@@ -0,0 +1,103 @@
+% This script saves the anfis model matrices to yaml file format
+
+model_path = '../models/anfis_direct';
+model_list = {direct_anfis.r, direct_anfis.vy};
+model_names = {"r", "vy"};
+
+input_norm = direct_anfis.norm;
+
+if ~exist(model_path, 'dir')
+    mkdir(model_path);
+end
+
+%% Save normalization parameters
+
+norm_file = fullfile(model_path, "normalization.yaml");
+fid = fopen(norm_file, 'w');
+
+fprintf(fid,"normalization:\n");
+
+fprintf(fid,"  mu: [");
+fprintf(fid,"%f ", input_norm.mu);
+fprintf(fid,"]\n");
+
+fprintf(fid,"  sigma: [");
+fprintf(fid,"%f ", input_norm.sigma);
+fprintf(fid,"]\n");
+
+fprintf(fid,"  x_min: [");
+fprintf(fid,"%f ", input_norm.x_min);
+fprintf(fid,"]\n");
+
+fprintf(fid,"  x_max: [");
+fprintf(fid,"%f ", input_norm.x_max);
+fprintf(fid,"]\n");
+
+fclose(fid);
+
+%% Export each ANFIS model
+
+for m = 1:length(model_list)
+
+    model = model_list{m};
+    fis = model.fis;
+
+    file = fullfile(model_path, model_names{m} + ".yaml");
+    fid = fopen(file,'w');
+
+    n_inputs = numel(fis.Inputs);
+    n_rules  = numel(fis.Rules);
+
+    fprintf(fid,"n_inputs: %d\n", n_inputs);
+    fprintf(fid,"n_rules: %d\n\n", n_rules);
+
+    %% Premise parameters
+
+    fprintf(fid,"premise:\n");
+
+    for i = 1:n_inputs
+
+        fprintf(fid,"  input%d:\n", i);
+
+        mfs = fis.Inputs(i).MembershipFunctions;
+
+        for j = 1:numel(mfs)
+
+            params = mfs(j).Parameters;
+
+            fprintf(fid,"    - [");
+
+            for k = 1:length(params)
+                fprintf(fid,"%f ", params(k));
+            end
+
+            fprintf(fid,"]\n");
+
+        end
+    end
+
+    fprintf(fid,"\n");
+
+    %% Consequent parameters
+
+    fprintf(fid,"consequent:\n");
+
+    for r = 1:n_rules
+
+        params = fis.Outputs(1).MembershipFunctions(r).Parameters;
+
+        fprintf(fid,"  - [");
+
+        for k = 1:length(params)
+            fprintf(fid,"%f ", params(k));
+        end
+
+        fprintf(fid,"]\n");
+
+    end
+
+    fclose(fid);
+
+end
+
+disp("ANFIS models exported successfully")
